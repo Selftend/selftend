690763434c69586ac3f3cf2bd11d6a50aea2b353
Vasil Yoshev <vasil.yoshev@gmail.com>
2026-05-16 23:32:04 +0300

    fix e2e test failure

--- test/e2e/create-gratitude-entry.e2e.test.ts
+++ test/e2e/create-gratitude-entry.e2e.test.ts
@@ -26,7 +26,7 @@ test.describe("create gratitude entry", () => {
 
     await page.goto("/tools/gratitude-log/new");
 
-    await page.getByPlaceholder("A small thing that mattered.").first().fill(item);
+    await page.getByRole("textbox", { name: "Gratitude 1" }).fill(item);
 
     await page.getByRole("button", { name: "Save", exact: true }).click();
 
